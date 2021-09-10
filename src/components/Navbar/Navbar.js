27a09639df0302309206f5b939bff323ec639138
ScooterMcgavin21 <Scott.skrobel@gmail.com>
2021-09-09 21:30:07 -0600

    Initial website files with navbar component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Nav } from './NavbarElements';
+
+function Navbar() {
+  return (
+    <>
+      <Nav>
+        Test
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
