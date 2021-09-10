e304c802e2642e5f6927c7ed59aafe4a3b00a87d
ScooterMcgavin21 <Scott.skrobel@gmail.com>
2021-09-10 15:15:26 -0600

    added Navbar components with links, updated global styles

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,72 @@
-import React from 'react';
-import { Nav, NavbarContainer, NavIcon, NavLogo } from './NavbarElements';
+import React, { useEffect, useState } from 'react';
+import { FaLayerGroup, FaTimes } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
+import { Button } from '../../globalStyles';
+import { MobileIcon, Nav, NavbarContainer, NavBtnLink, NavIcon, NavItem, NavItemBtn, NavLinks, NavLogo, NavMenu } from './NavbarElements';
 
 function Navbar() {
+  /* click and button state variables Defined to update Hamburger menu */
+  const [click, setClick ] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const showButton = () => {
+    if(window.innerWidth <= 960) {
+      setButton(false)
+    } else {
+      setButton(true)
+    }
+  }
+  /** Hook for button component  */
+  useEffect(() => {
+    showButton()
+  }, [])
+  window.addEventListener('resize', showButton);
   return (
     <>
+    <IconContext.Provider value={{ color: '#fff' }}>
       <Nav>
         <NavbarContainer>
           <NavLogo to='/' >
             <NavIcon />
             TaxiStake
           </NavLogo>
+          <MobileIcon onClick={handleClick}>
+            {click ? <FaTimes /> : <FaLayerGroup />}
+          </MobileIcon>
+          <NavMenu onClick={handleClick} click={click}>
+            <NavItem>
+              <NavLinks to='/'>
+                Home
+              </NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to='/services'>
+                Services
+              </NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to='/products'>
+                Products
+              </NavLinks>
+            </NavItem>
+            <NavItemBtn>
+              {button ? (
+                <NavBtnLink to='/sign-up'>
+                  <Button primary>SIGN UP</Button>
+                </NavBtnLink>
+              ) : (
+                <NavBtnLink to='/sign-up'>
+                  <Button fontBig primary>
+                    SIGN UP
+                  </Button>
+                </NavBtnLink>
+              )}
+            </NavItemBtn>
+          </NavMenu>
         </NavbarContainer>
       </Nav>
+      </IconContext.Provider>
     </>
   );
 };
--- src/globalStyles.js
+++ src/globalStyles.js
@@ -12,18 +12,42 @@ const GlobalStyle = createGlobalStyle`
 
 /** GlobalStyle Container rendering <div> tag */
 export const Container = styled.div`
-z-index: 1;
-width: 100%;
-max-width: 1300px;
-margin-right: auto;
-margin-left: auto;
-padding-left: 50px;
-padding-right: 50px;
+  z-index: 1;
+  width: 100%;
+  max-width: 1300px;
+  margin-right: auto;
+  margin-left: auto;
+  padding-left: 50px;
+  padding-right: 50px;
 
-@media screen and (max-width: 991px) {
-  padding-left: 30px;
-  padding-right: 30px;
-}
+  @media screen and (max-width: 991px) {
+    padding-left: 30px;
+    padding-right: 30px;
+  }
+`;
+
+/** Button Container rendering button tag */
+export const Button = styled.button`
+  border-radius: 4px;
+  // If primary colors set, use it
+  background: ${({primary}) => (primary ? '#4B59F7' : '#0467FB')};
+  white-space: nowrap;
+  padding: ${({big}) => (big ? '12px 64px' : '10px 20px')};
+  color: #fff;
+  font-size: ${({fontBig}) => (fontBig ? '20px' : '16px')};
+  outline: none;
+  border: none;
+  cursor: pointer;
+
+  &:hover {
+    transition: all 0.3s ease-out;
+    background: #fff;
+    background: ${({primary}) => (primary ? '#0467FB' : '#4B59F7')};
+  }
+
+  @media screen and (max-width: 960px) {
+    width: 100%;
+  }
 `;
 
 export default GlobalStyle;
